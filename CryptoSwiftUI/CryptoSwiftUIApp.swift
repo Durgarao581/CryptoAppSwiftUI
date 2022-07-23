aca128103a3b9fe53d24457a97bb3e1551fece6f
Durgarao Kottapalli <durgarao@gisfy.co.in>
2022-07-23 15:16:24 +0530

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Ganga Durgarao Kothapalli on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
